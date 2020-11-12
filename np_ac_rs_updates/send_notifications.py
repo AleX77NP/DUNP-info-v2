e471685dd7e1de1272cf8787cb1c8983870911b3
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2020-11-12 22:25:01 +0100

    fix oko filtera

--- np_ac_rs_updates/send_notifications.py
+++ np_ac_rs_updates/send_notifications.py
@@ -4,6 +4,8 @@ from .models import Student
 
 def send_notification_filter(title, message, tip): 
     try:
+        print(tip)
+        print(title)
         emailovi = Student.objects.filter(pretplate__icontains=tip).values_list('email', flat=True) #uzmi studente koji medju pretplatama imaju tip vesti koja stize, i njima posalji 
         studenti = Student.objects.filter(pretplate__icontains=tip)
 
@@ -14,15 +16,13 @@ def send_notification_filter(title, message, tip):
         if tip == "vesti" or tip == "obavestenja" or tip == "instagram":
             devices = FCMDevice.objects.filter(name__in=lista)
             devices.send_message(title=title, body=message, sound=True)
+
         else:
             for student in studenti:
                 smerovi = student.smerovi.split('_')
                 departmani = student.departmani.split('_')
-            
-                print(smerovi)
-                print(departmani)
 
-                if any(smer in message for smer in smerovi) or any(dep in message for dep in departmani):
+                if any(smer in title for smer in smerovi) or any(dep in title for dep in departmani):
                     device = FCMDevice.objects.filter(name=student.email)
                     device.send_message(title=title, body=message, sound=True)
                 
